6c8fd1ab25ab771e63fcf7af83dc30f4e3133bd5
JayramNanoskar <jayram.n978@gmail.com>
2020-11-16 21:32:25 +0530

    creating a service

--- third-project/src/app/app.module.ts
+++ third-project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ViewModule } from './view/view.module';
+import { TestService } from './test.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { ViewModule } from './view/view.module';
     BrowserModule,
     ViewModule // need to import module prior using it
   ],
-  providers: [],
+  providers: [TestService], // service need to be add here so that it becomes part of app module and can be used by components resides in app module
   bootstrap: [AppComponent]
 })
 export class AppModule { }
