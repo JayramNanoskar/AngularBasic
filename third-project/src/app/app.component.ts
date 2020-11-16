db415f44556c06d14dd68a33d3ab8678d54bc715
JayramNanoskar <jayram.n978@gmail.com>
2020-11-16 22:55:39 +0530

    making REST calls with HttpClient service

--- third-project/src/app/app.component.ts
+++ third-project/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { TestService } from './test.service';
 
@@ -8,8 +9,14 @@ import { TestService } from './test.service';
 })
 export class AppComponent {
   
-  constructor(private srv: TestService){ // Dependency injection using constructor arguments and get hold on it using member variable
+  constructor(private srv: TestService, private http: HttpClient){ // Dependency injection using constructor arguments and get hold on it using member variable
     // let srv = new TestService(); // need to create instance of a require service here but other alternative is there i.e. Dependency injection(asking framework to provide instance) as above inside constructor
     srv.printToConsole("Welcome!!");
+  }
+
+  ngOnInit(){
+    let obs = this.http.get("https://api.github.com/users/jayramnanoskar"); // can do Async operation using Observable instead of promises from Angular2
+    console.log("obs = "+obs); //print Observable object
+    obs.subscribe((response) => console.log(response));
   }
 }
--- third-project/src/app/app.module.ts
+++ third-project/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ViewModule } from './view/view.module';
 import { TestService } from './test.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { TestService } from './test.service';
   ],
   imports: [
     BrowserModule,
-    ViewModule // need to import module prior using it
+    ViewModule, // need to import module prior using it
+    HttpClientModule // for Rest calls
   ],
   providers: [TestService], // service need to be add here so that it becomes part of app module and can be used by components resides in app module
   bootstrap: [AppComponent]
