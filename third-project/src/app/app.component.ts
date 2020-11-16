e273ee9a37d6d930d6c14e4de41c36ca0c11cfee
JayramNanoskar <jayram.n978@gmail.com>
2020-11-16 22:03:15 +0530

    using dependency injection

--- third-project/src/app/app.component.ts
+++ third-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'third-project';
+  
+  constructor(private srv: TestService){ // Dependency injection using constructor arguments and get hold on it using member variable
+    // let srv = new TestService(); // need to create instance of a require service here but other alternative is there i.e. Dependency injection(asking framework to provide instance) as above inside constructor
+    srv.printToConsole("Welcome!!");
+  }
 }
