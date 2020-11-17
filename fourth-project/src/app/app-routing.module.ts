868782268201215588cece31f4d61d5847a515d2
JayramNanoskar <jayram.n978@gmail.com>
2020-11-17 16:08:22 +0530

    creating routes, route redirects and wildcards

--- fourth-project/src/app/app-routing.module.ts
+++ fourth-project/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "settings", component: SettingsComponent},
+  {path: "**", component: PageNotFoundComponent} // wildcard - to match anything
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fourth-project/src/app/app.module.ts
+++ fourth-project/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SettingsComponent
+    HomeComponent, // component entry
+    SettingsComponent, PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // module for routing 
   ],
   providers: [],
   bootstrap: [AppComponent]
